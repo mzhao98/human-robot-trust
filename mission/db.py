9418f31f14a6551a1822979e9c298734706d2196
mzhao98 <michelledzhao@gmail.com>
2021-10-14 09:46:41 -0400

    update files

--- mission/db.py
+++ mission/db.py
@@ -2,14 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# user = "root"
-# password = "Turbotax98!"
-# host = "localhost:3306"
-# db_name = "sar_game"
-user = "admin"
-password = "michelle"
-host = "testdb.cqnxcsogehau.us-east-1.rds.amazonaws.com"
+user = "username"
+password = "password"
+host = "localhost:3306"
 db_name = "sargame"
+
 
 DATABASE_URL = 'mysql+mysqlconnector://%s:%s@%s/%s?charset=utf8' % (
     user,
--- mission/static/assets/tilemaps/minecraft_tileset_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="minecraft_tileset_1" tilewidth="30" tileheight="30" tilecount="3" columns="3">
- <image source="tiles/dirt.png" width="90" height="30"/>
-</tileset>
